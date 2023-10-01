a92ea4b7db133f71804319230eab410130d5a20a
soso0024 <131151398+soso0024@users.noreply.github.com>
2023-10-01 21:30:33 +0200

    add program

---
+++ find_the_number/main.cpp
@@ -0,0 +1,49 @@
+#include <iostream>
+#include <ctime>
+
+using namespace std;
+
+bool isNumber(const string &str){
+    for(char c: str){
+        if(!isdigit(c)) return false;
+    }
+    return true;
+}
+
+int main(){
+    srand(time(0));
+    int random = rand() % 100 + 1;
+    // cout << "randomNumber: " << random << "\n";
+    cout << "userInput: ";
+
+    while(1){
+        string userInput;
+        cin >> userInput;
+
+        if(isNumber(userInput)){
+            int num = atoi(userInput.c_str());
+
+            if(num > random){
+                cout << "smaller"
+                     << "\n" << "\n"
+                     << "userInput: ";
+            }
+
+            if(num == random){
+                cout << "Correct!" << "\n";
+                break;
+            }
+
+            if(num < random){
+                cout << "bigger"
+                     << "\n" << "\n"
+                     << "userInput: ";
+            }
+        }else{
+            cout << "You should input the Number!"
+                 << "\n" << "\n"
+                 << "userInput";
+        }
+    }
+    return 0;
+}
