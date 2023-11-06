cfa06ddc16ac65986b43048b3c6ede3c3f7d2c06
soso0024 <131151398+soso0024@users.noreply.github.com>
2023-11-06 16:03:17 +0100

    change the program of battleship

--- Battleship/include/aiPlayer.hpp
+++ Battleship/include/aiPlayer.hpp
@@ -7,6 +7,8 @@ class aiPlayer : public Player
 {
     std::vector<std::pair<int, int>> potential_targets;
 
+    std::pair<int, int> first_hit_position = {-1, -1}; // 最初にヒットした位置を記録する変数
+
     // 特定のセルが既に攻撃されたかどうかを確認
     bool isAttacked(int row, int col) const
     {
@@ -19,15 +21,48 @@ class aiPlayer : public Player
         return row >= 0 && row < m_grid_rows && col >= 0 && col < m_grid_cols;
     }
 
-    // 選択したセルが戦艦だと`true`を返す
     bool check_hit(int row, int col)
     {
-        return m_grid[row][col] != '~';
+        if (m_grid[row][col] != '~')
+        {
+            std::cout << "Im here 1" << std::endl;
+
+            if (first_hit_position.first == -1 && first_hit_position.second == -1)
+            {
+                first_hit_position = {row, col}; // 最初にヒットした位置を更新する
+
+                std::cout << "最初にヒットした位置を記録したよ" << std::endl;
+            }
+            return true;
+        }
+        return false;
     }
 
     bool is_ship_sunk(int row, int col) const
     {
-        return false;
+        // Check if the cell at (row, col) has been attacked
+        if (!attacks_grid[row][col])
+        {
+            return false; // If the cell has not been attacked, the ship cannot be sunk
+        }
+
+        // Retrieve the ID of the ship at the attacked cell
+        char shipID = m_grid[row][col];
+
+        // Check all cells for this ship ID
+        for (unsigned int r = 0; r < m_grid_rows; ++r)
+        {
+            for (unsigned int c = 0; c < m_grid_cols; ++c)
+            {
+                if (m_grid[r][c] == shipID && !attacks_grid[r][c]) // !attacks_grid[][]で攻撃されていないセル
+                {
+                    return false; // If any cell with this ship ID has not been attacked, the ship is not sunk
+                }
+            }
+        }
+
+        // If all cells with this ship ID have been attacked, the ship is sunk
+        return true;
     }
 
     // Helper function to update potential targets around the last hit position
@@ -59,6 +94,7 @@ public:
     void logical_attack()
     {
         int row, col;
+
         // Update potential targets around the last hit position
         if (last_hit_position.first != -1 && last_hit_position.second != -1)
         {
@@ -75,12 +111,29 @@ public:
         }
         else
         {
-            // Select a new random position that has not been attacked yet
-            do
+            // If there are no potential targets and the first hit position is valid
+            if (first_hit_position.first != -1 && first_hit_position.second != -1)
             {
-                row = getRandomInt(m_grid_rows);
-                col = getRandomInt(m_grid_cols);
-            } while (isAttacked(row, col)); // Avoid already attacked positions
+                // Reset last hit to first hit and update potential targets around it
+                last_hit_position = first_hit_position;
+                update_potential_targets();
+                if (!potential_targets.empty())
+                {
+                    int rand_index = getRandomInt(potential_targets.size());
+                    auto target = potential_targets[rand_index];
+                    row = target.first;  // Updated row value
+                    col = target.second; // Updated column value
+                }
+            }
+            // If there are no potential targets at all, select a new random position
+            if (potential_targets.empty())
+            {
+                do
+                {
+                    row = getRandomInt(m_grid_rows);
+                    col = getRandomInt(m_grid_cols);
+                } while (isAttacked(row, col)); // Avoid already attacked positions
+            }
         }
 
         print_ai_selection(row, col); // Display the correct row and column
@@ -91,7 +144,11 @@ public:
         {
             if (is_ship_sunk(row, col))
             {
-                last_hit_position = {-1, -1};
+                std::cout << "船が沈んでいるようだから，リセットするね" << std::endl;
+                potential_targets.clear();
+
+                first_hit_position = {-1, -1}; // ここで最初にヒットした位置をリセットする
+                last_hit_position = {-1, -1};  // ここで最後にヒットした位置をリセットする
             }
             else
             {
